fcb08c7bdf95b737933ea9bdd6c4c4a4f9bf317b
thasuohm <thasuohm@gmail.com>
2021-09-13 16:41:07 +0700

    table pagination userlist and table basedata + add color

--- src/components/pages/ListBaseDataPage.js
+++ src/components/pages/ListBaseDataPage.js
@@ -1,44 +1,290 @@
-import { Container, Grid } from '@material-ui/core';
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import ColorCard from '../ui/ColorCard';
-import TypeCard from '../ui/TypeCard';
+import {
+  Container,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ListBaseDataPage = () => {
+  const [color, setColor] = useState([]);
+  const [type, setType] = useState([]);
+  const [addColor, setAddColor] = useState(false);
+  const [addType, setAddType] = useState(false);
+  const [colorToAdd, setColorToAdd] = useState("");
+  const [typeToAdd, setTypeToAdd] = useState("");
 
-    const [color , setColor] = useState([]);
-    const [type , setType] = useState([]);
+  useEffect(() => {
+    function getData() {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/colors`)
+        .then((res) => setColor(res.data));
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/brands`)
+        .then((res) => setType(res.data));
+    }
+    getData();
+  }, [color, type]);
 
-    useEffect(() => {
+  const delColor = (id) => {
+    // axios.delete(`${process.env.REACT_APP_API_URL}/colordelete/${id}`);
+  };
 
-        axios.get(`${process.env.REACT_APP_API_URL}/colors`).then(res => setColor(res.data))
-        axios.get(`${process.env.REACT_APP_API_URL}/brands`).then(res => setType(res.data))
-        },[])
+  const delType = (id) => {
+    //   axios.delete(`${process.env.REACT_APP_API_URL}/typedelete/${id}`);
+  };
+
+  const handleColor = (e) => {
+    setColorToAdd(e.target.value);
+  };
+
+  const handleType = (e) => {
+    setTypeToAdd(e.target.value);
+  };
+
+  const submitColor = () => {
+    const json = JSON.stringify({ colorName: colorToAdd });
+    try { axios
+        .post(`${process.env.REACT_APP_API_URL}/addcolor`, json, {
+          headers: {
+            "Content-Type": "application/json",
+          }
+        })
+        .then(axios.get(`${process.env.REACT_APP_API_URL}/colors`)
+        .then((res) => setColor(res.data))
+        .then(setColorToAdd(""))
+        .then(alert("add color success")));
+     }
+       catch (err) {
+        alert(err);
+
+       }
+    
+  };
+
+  const submitType = () => {
+    const json = JSON.stringify({ name: typeToAdd });
+
+    try {
+      axios
+        .post(`${process.env.REACT_APP_API_URL}/addtype`, json, {
+            headers: {
+              "Content-Type": "application/json",
+            }
+          })
+          .then(axios.get(`${process.env.REACT_APP_API_URL}/brands`)
+          .then((res) => setType(res.data))
+          .then(setTypeToAdd(""))
+          .then(alert("add Type success")));
+        
+    }catch (err) {
+        alert(err);
+
+    }
 
 
-    return (
-        <Container maxWidth='lg' style={{ marginTop: 10+'px'}}>
-             <div style={{ backgroundColor : 'white', padding: 15+'px', borderRadius : 10+'px'}}> 
-             <div style={{ fontWeight: 600 , fontSize: 24+'px', marginBottom : 50+'px'}}>All Colors and Types</div>
-             <Grid container>
-                <Grid item xs={12} md={6}>
-                <div style={{fontWeight: 600 , fontSize:20+'px'}}> Colors </div>
-                { color.map( col => {
 
-                    return <ColorCard key={col.colorId} color={col}/>
-                })}</Grid>
 
-                <Grid item xs={12} md={6}>
-                <div style={{fontWeight: 600, fontSize:20+'px'}}> Types </div>
-                { type.map( type => {
-                return <TypeCard key={type.typeId} type={type} />
-                    })}</Grid>
-               </Grid> 
-             </div>
-           
-        </Container>
-    )
-}
+  };
 
+  return (
+    <Container maxWidth="lg" style={{ marginTop: 10 + "px" }}>
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: 15 + "px",
+          borderRadius: 10 + "px",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: 600,
+            fontSize: 24 + "px",
+            marginBottom: 50 + "px",
+          }}
+        >
+          All Colors and Types
+        </div>
+        <Grid container>
+          <Grid item xs={12} md={6} style={{ paddingBottom: 50 + "px" }}>
+            <div
+              style={{
+                fontWeight: 600,
+                fontSize: 20 + "px",
+                paddingLeft: 35 + "px",
+              }}
+            >
+              {" "}
+              Colors{" "}
+              <button
+                className="AddButton"
+                style={{ float: "right", marginRight: 30 + "px" }}
+                onClick={() => setAddColor(!addColor)}
+              >
+                ADD COLOR! +
+              </button>{" "}
+            </div>
+            {addColor && (
+              <div
+                style={{
+                  borderRadius: 5 + "px",
+                  borderStyle: "solid",
+                  borderWidth: 1 + "px",
+                  padding: 10 + "px",
+                  borderColor: "#545454",
+                  width: 90 + "%",
+                  marginTop: 10 + "px",
+                  marginBottom: 10 + "px",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              >
+                <TextField
+                  size="small"
+                  variant="outlined"
+                  onChange={handleColor}
+                  label="colorName"
+                  value={colorToAdd}
+                />{" "}
+                <button
+                  className="InfoButton"
+                  style={{ float: "right" }}
+                  onClick={submitColor}
+                >
+                  Submit Color
+                </button>
+              </div>
+            )}
+
+            <Table style={{ width: 90 + "%", margin: "auto" }}>
+              <TableHead style={{ backgroundColor: "#3f51b5" }}>
+                <TableRow>
+                  <TableCell style={{ color: "white" }} align="right">
+                    ID
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="right">
+                    color name
+                  </TableCell>
+                  <TableCell
+                    style={{ color: "white" }}
+                    align="right"
+                  ></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {color.map((col) => {
+                  return (
+                    <TableRow key={col.colorId}>
+                      <TableCell align="right">{col.colorId}</TableCell>
+                      <TableCell align="right">{col.colorName}</TableCell>
+                      <TableCell align="right">
+                        <button
+                          className="delFromCart"
+                          onClick={delColor(col.colorId)}
+                        >
+                          DELETE
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <div
+              style={{
+                fontWeight: 600,
+                fontSize: 20 + "px",
+                paddingLeft: 35 + "px",
+              }}
+            >
+              {" "}
+              Types{" "}
+              <button
+                className="AddButton"
+                style={{ float: "right", marginRight: 30 + "px" }}
+                onClick={() => setAddType(!addType)}
+              >
+                ADD TYPE! +
+              </button>
+            </div>
+            {addType && (
+              <div
+                style={{
+                  borderRadius: 5 + "px",
+                  borderStyle: "solid",
+                  borderWidth: 1 + "px",
+                  padding: 10 + "px",
+                  borderColor: "#545454",
+                  width: 90 + "%",
+                  marginTop: 10 + "px",
+                  marginBottom: 10 + "px",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              >
+                <TextField
+                  size="small"
+                  variant="outlined"
+                  onChange={handleType}
+                  label="TypeName"
+                />{" "}
+                <button
+                  className="InfoButton"
+                  style={{ float: "right" }}
+                  onClick={submitType}
+                >
+                  Submit Type
+                </button>
+              </div>
+            )}
+
+            <Table style={{ width: 90 + "%", margin: "auto" }}>
+              <TableHead style={{ backgroundColor: "#3f51b5" }}>
+                <TableRow>
+                  <TableCell style={{ color: "white" }} align="right">
+                    ID
+                  </TableCell>
+                  <TableCell style={{ color: "white" }} align="right">
+                    Type name
+                  </TableCell>
+                  <TableCell
+                    style={{ color: "white" }}
+                    align="right"
+                  ></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {type.map((type) => {
+                  return (
+                    <TableRow key={type.typeId}>
+                      <TableCell align="right">{type.typeId}</TableCell>
+                      <TableCell align="right">{type.name}</TableCell>
+                      <TableCell align="right">
+                        <button
+                          className="delFromCart"
+                          onClick={delType(type.typeId)}
+                        >
+                          DELETE
+                        </button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
+};
 
 export default ListBaseDataPage;
--- src/components/pages/UserListPage.js
+++ src/components/pages/UserListPage.js
@@ -1,94 +1,121 @@
-import { Container, Table, TableBody, TableCell, TableHead, TableRow, TableFooter } from '@material-ui/core'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableFooter,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import TablePagination from "../ui/TablePagination";
 
-
-
 const UserListPage = () => {
-const [user , setUser] = useState([]);
-const [page , setPage] = useState(0);
-const [rowsPerPage] = useState(5);
+  const [user, setUser] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage] = useState(5);
 
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/setest`)
+      .then((res) => setUser(res.data));
+  }, []);
 
-useEffect(() => {
+  const delUser = (id) => {
+    try {
+      axios.delete(`${process.env.REACT_APP_API_URL}/use/delete/${id}`);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-axios.get(`${process.env.REACT_APP_API_URL}/setest`).then(res => setUser(res.data))
+  const handleChangePage = (e, newPage) => {
+    setPage(newPage);
+  };
 
-},[])
-
-const delUser = (id) => {
-try{
-axios.delete(`${process.env.REACT_APP_API_URL}/use/delete/${id}`)
-}
-catch (err){
-
-    alert(err);
-
-}
-
-}
-
-const handleChangePage = (e,newPage) => {
-setPage(newPage)
-
-
-}
-
-
-
-    return (
-        <Container maxWidth='lg' style={{ marginTop: 10+'px'}}>
-        <div style={{ backgroundColor : 'white', padding: 40+'px', borderRadius : 10+'px'}}> 
-        <div style={{ fontWeight: 600 , fontSize: 24+'px', marginBottom : 50+'px'}}>All Users</div>
-        <Table style={{width : 95+'%',margin : 'auto'}}>
-            <TableHead>
-                <TableRow style={{backgroundColor : '#3f51b5'}}>
-            <TableCell style={{color: 'white' }} align="right">ID</TableCell>
-            <TableCell style={{color: 'white' }} align="right">Username</TableCell>
-            <TableCell style={{color: 'white' }} align="right">Role</TableCell>
-            <TableCell style={{color: 'white' }} align="right"></TableCell>
-
-                </TableRow>
-            </TableHead>
-            <TableBody>
-
-          
-            {(rowsPerPage) > 0 ? user.slice(page * rowsPerPage,page* rowsPerPage + rowsPerPage).map( user => {
-            return <TableRow>
-                <TableCell align="right">{user.userId}</TableCell>
-                <TableCell align="right">{user.userName}</TableCell>
-                <TableCell align="right">{user.role}</TableCell>
-              {user.role.toLowerCase()!== 'admin' && <TableCell align="right"> <button className="AddButton">Edit</button> <button className="delFromCart" onClick={delUser(user.userId)}>DELETE</button></TableCell>} 
-
-            </TableRow>
-
-        }) : <TableRow> <TableCell/></TableRow>}
-
-    
-
-        
-        </TableBody>
-
-        <TableFooter >
-
-            
-      <TableRow>
-      <TableCell colSpan={4} align="right">
-         
-     <TablePagination onPageChange={handleChangePage} count={user.length} page={page} rowsPerPage={rowsPerPage}></TablePagination>
-</TableCell>
-        </TableRow>
-
-        </TableFooter>
-
-
-
-
-        </Table>
+  return (
+    <Container maxWidth="lg" style={{ marginTop: 10 + "px" }}>
+      <div
+        style={{
+          backgroundColor: "white",
+          padding: 40 + "px",
+          borderRadius: 10 + "px",
+        }}
+      >
+        <div
+          style={{
+            fontWeight: 600,
+            fontSize: 24 + "px",
+            marginBottom: 50 + "px",
+          }}
+        >
+          All Users
         </div>
-    </Container>
-    )
-}
+        <Table style={{ width: 95 + "%", margin: "auto" }}>
+          <TableHead>
+            <TableRow style={{ backgroundColor: "#3f51b5" }}>
+              <TableCell style={{ color: "white" }} align="right">
+                ID
+              </TableCell>
+              <TableCell style={{ color: "white" }} align="right">
+                Username
+              </TableCell>
+              <TableCell style={{ color: "white" }} align="right">
+                Role
+              </TableCell>
+              <TableCell style={{ color: "white" }} align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rowsPerPage > 0 ? (
+              user
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((user) => {
+                  return (
+                    <TableRow key={user.userId}>
+                      <TableCell align="right">{user.userId}</TableCell>
+                      <TableCell align="right">{user.userName}</TableCell>
+                      <TableCell align="right">{user.role}</TableCell>
+                      {user.role.toLowerCase() !== "admin" && (
+                        <TableCell align="right">
+                          {" "}
+                          <button className="AddButton">Edit</button>{" "}
+                          <button
+                            className="delFromCart"
+                            onClick={delUser(user.userId)}
+                          >
+                            DELETE
+                          </button>
+                        </TableCell>
+                      )}
+                    </TableRow>
+                  );
+                })
+            ) : (
+              <TableRow>
+                {" "}
+                <TableCell />
+              </TableRow>
+            )}
+          </TableBody>
 
-export default UserListPage
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={4} align="right">
+                <TablePagination
+                  onPageChange={handleChangePage}
+                  count={user.length}
+                  page={page}
+                  rowsPerPage={rowsPerPage}
+                ></TablePagination>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </Container>
+  );
+};
+
+export default UserListPage;
