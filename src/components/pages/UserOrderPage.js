f7d207a8acb6effa5d496e1281ff0dffd71965bc
thasuohm <thasuohm@gmail.com>
2021-10-01 21:16:12 +0700

    try fix get order

--- src/components/pages/UserOrderPage.js
+++ src/components/pages/UserOrderPage.js
@@ -17,7 +17,8 @@ const UserOrderPage = () => {
   const getMyOrderHistory = () => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/getuserorder/1`)
-      .then((res) => setMyOrder(res.data))
+      .then((res) => res.json())
+      .then(res => setMyOrder(res.data))
       .catch((err) => {
         setDialogHeader("Error");
         setDialogContent(err.message);
@@ -53,110 +54,109 @@ const UserOrderPage = () => {
         }}
       >
         <h2>ประวัติการสั่งซื้อ </h2>
-        {myOrder &&
-          myOrder.map((order) => {
-            return (
-              <div
-                className="orderCard"
-                key={order.userOrderId}
+        {myOrder.map((order) => {
+          return (
+            <div
+              className="orderCard"
+              key={order.userOrderId}
+              style={{
+                paddingLeft: 35 + "px",
+                paddingRight: 35 + "px",
+                paddingBottom: 35 + "px",
+                paddingTop: 10 + "px",
+                color: "#181818",
+                marginBottom: 30 + "px",
+                backgroundColor: "white",
+                borderRadius: 10 + "px",
+                borderWidth: "1px",
+                borderStyle: "solid",
+                borderColor: "#d1eafd",
+                boxShadow: " 7px 4px #d1eafd",
+              }}
+            >
+              <h3
                 style={{
-                  paddingLeft: 35 + "px",
-                  paddingRight: 35 + "px",
-                  paddingBottom: 35 + "px",
-                  paddingTop: 10 + "px",
-                  color: "#181818",
-                  marginBottom: 30 + "px",
-                  backgroundColor: "white",
-                  borderRadius: 10 + "px",
-                  borderWidth: "1px",
                   borderStyle: "solid",
-                  borderColor: "#d1eafd",
-                  boxShadow: " 7px 4px #d1eafd",
+                  borderWidth: "0 0 1px 0",
+                  marginRight: "20px",
+                  borderColor: "#181818",
                 }}
               >
-                <h3
-                  style={{
-                    borderStyle: "solid",
-                    borderWidth: "0 0 1px 0",
-                    marginRight: "20px",
-                    borderColor: "#181818",
-                  }}
-                >
-                  หมายเลขคำสั่งซื้อ #{order.userOrderId}
-                </h3>
-                <h4>วันที่สั่งซื้อ {order.date}</h4>
-                <Grid container>
-                  {order.orderDetails.map((odt) => {
-                    return (
-                      <Grid
-                        item
-                        xs={12}
-                        sm={6}
-                        md={5}
-                        key={odt.orderDetailId}
-                        style={{
-                          margin: 10 + "px",
-                          borderRadius: "8px",
-                          borderStyle: "solid",
-                          borderWidth: "0.5px",
-                          borderColor: "#d1eafd",
-                          boxShadow: " 5px 5px #d1eafd",
-                        }}
-                      >
-                        <Grid container justifyContent="center">
-                          <Grid item xs={8} sm={7} md={5}>
-                            <img
-                              src={`${process.env.REACT_APP_API_URL}/getImage/${odt.product.imageName}`}
-                              alt="productInOrder"
-                              className="imageProduct2"
-                              style={{ paddingTop: "35px", paddingLeft: "3px" }}
-                            />
-                          </Grid>
-                          <Grid item xs={10} md={8} lg={6}>
-                            <h3
+                หมายเลขคำสั่งซื้อ #{order.userOrderId}
+              </h3>
+              <h4>วันที่สั่งซื้อ {order.date}</h4>
+              <Grid container>
+                {order.orderDetails.map((odt) => {
+                  return (
+                    <Grid
+                      item
+                      xs={12}
+                      sm={6}
+                      md={5}
+                      key={odt.orderDetailId}
+                      style={{
+                        margin: 10 + "px",
+                        borderRadius: "8px",
+                        borderStyle: "solid",
+                        borderWidth: "0.5px",
+                        borderColor: "#d1eafd",
+                        boxShadow: " 5px 5px #d1eafd",
+                      }}
+                    >
+                      <Grid container justifyContent="center">
+                        <Grid item xs={8} sm={7} md={5}>
+                          <img
+                            src={`${process.env.REACT_APP_API_URL}/getImage/${odt.product.imageName}`}
+                            alt="productInOrder"
+                            className="imageProduct2"
+                            style={{ paddingTop: "35px", paddingLeft: "3px" }}
+                          />
+                        </Grid>
+                        <Grid item xs={10} md={8} lg={6}>
+                          <h3
+                            style={{
+                              borderStyle: "solid",
+                              borderWidth: "0 0 1px 0",
+                              marginRight: "20px",
+                              borderColor: "#181818",
+                            }}
+                          >
+                            <Link
+                              to={`/product/${odt.product.productId}`}
+                              className="hoverChangeToNavBarColor"
                               style={{
-                                borderStyle: "solid",
-                                borderWidth: "0 0 1px 0",
-                                marginRight: "20px",
-                                borderColor: "#181818",
+                                marginRight: 30 + "px",
+                                textDecoration: "none",
+                                fontWeight: 600,
                               }}
                             >
-                              <Link
-                                to={`/product/${odt.product.productId}`}
-                                className="hoverChangeToNavBarColor"
-                                style={{
-                                  marginRight: 30 + "px",
-                                  textDecoration: "none",
-                                  fontWeight: 600,
-                                }}
-                              >
-                                {odt.product.name}
-                              </Link>
-                            </h3>
+                              {odt.product.name}
+                            </Link>
+                          </h3>
 
-                            <span>จำนวน : {odt.quantity} ชิ้น /</span>
-                            <span> สี : {odt.color.colorName}</span>
-                            <h4> รวม : {odt.totalPrice} บาท</h4>
-                            <button
-                              className="AddButton"
-                              style={{
-                                marginTop: -5 + "px",
-                                marginBottom: 20 + "px",
-                                paddingBottom: "5px",
-                              }}
-                              onClick={handleComment}
-                            >
-                              แสดงความคิดเห็น
-                            </button>
-                          </Grid>
+                          <span>จำนวน : {odt.quantity} ชิ้น /</span>
+                          <span> สี : {odt.color.colorName}</span>
+                          <h4> รวม : {odt.totalPrice} บาท</h4>
+                          <button
+                            className="AddButton"
+                            style={{
+                              marginTop: -5 + "px",
+                              marginBottom: 20 + "px",
+                              paddingBottom: "5px",
+                            }}
+                            onClick={handleComment}
+                          >
+                            แสดงความคิดเห็น
+                          </button>
                         </Grid>
                       </Grid>
-                    );
-                  })}
-                </Grid>
-              </div>
-            );
-          })}
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </div>
+          );
+        })}
       </Container>
     </>
   );
